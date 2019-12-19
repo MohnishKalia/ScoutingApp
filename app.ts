02808079f16821565534a7eeb2c9adf1511996d9
MohnishKalia <mohnish.kalia@gmail.com>
2019-12-19 15:08:25 -0600

    Add bootstrap and access api

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 const app = express();
 require('dotenv').config();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
@@ -12,13 +12,26 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/endpoint', (req, res) => {
-    res.render('endpoint');
+app.get('/:endpoint', (req, res) => {
+    res.render(req.params.endpoint);
 });
 
+/*
+posting endpoints:
+   /team/${team_key}/events/${year} (/keys ||/statuses )
+   /events/${year}
+   /event/${event_key}/teams (/keys)
+   /team/${team_key}/matches/${year}
+   /event/${event_key} (/oprs ||/rankings ||/insights )
+   /team/${team_key}/event/${event_key}/matches
+
+   TODO: alias these with names we can use everywhere like d.ts, function names, etc.
+*/
+
 app.post('/endpoint', async (req, res) => {
-    const {team_key, year} = req.body;
-    const response = await getTBA(`/team/${team_key}/matches/${year}`);
+    // TODO: add a thing for optional endpoint ending
+    const { team_key, year, aep } = req.body;
+    const response = await getTBA(`/team/${team_key}/events/${year}${aep}`);
     res.json(response);
 });
 
--- out/app.js
+++ out/app.js
@@ -7,18 +7,28 @@ const express_1 = __importDefault(require("express"));
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const app = express_1.default();
 require('dotenv').config();
-app.use(express_1.default.urlencoded());
+app.use(express_1.default.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.render('index');
 });
-app.get('/endpoint', (req, res) => {
-    res.render('endpoint');
+app.get('/:endpoint', (req, res) => {
+    res.render(req.params.endpoint);
 });
+/*
+posting endpoints:
+   /team/${team_key}/events/${year} (/keys ||/statuses )
+   /events/${year}
+   /event/${event_key}/teams (/keys)
+   /team/${team_key}/matches/${year}
+   /event/${event_key} (/oprs ||/rankings ||/insights )
+   /team/${team_key}/event/${event_key}/matches
+*/
 app.post('/endpoint', async (req, res) => {
-    const { team_key, year } = req.body;
-    const response = await getTBA(`/team/${team_key}/matches/${year}`);
+    // TODO: add a thing for optional endpoint ending
+    const { team_key, year, aep } = req.body;
+    const response = await getTBA(`/team/${team_key}/matches/${year}${aep}`);
     res.json(response);
 });
 const getTBA = async (endpoint) => {
