b3ae05699e5d006627004769eb740ce74ca30bc9
MohnishKalia <mohnish.kalia@gmail.com>
2019-12-19 16:07:47 -0600

    Configure build

--- out/app.js
+++ out/app.js
@@ -16,19 +16,9 @@ app.get('/', (req, res) => {
 app.get('/:endpoint', (req, res) => {
     res.render(req.params.endpoint);
 });
-/*
-posting endpoints:
-   /team/${team_key}/events/${year} (/keys ||/statuses )
-   /events/${year}
-   /event/${event_key}/teams (/keys)
-   /team/${team_key}/matches/${year}
-   /event/${event_key} (/oprs ||/rankings ||/insights )
-   /team/${team_key}/event/${event_key}/matches
-*/
 app.post('/endpoint', async (req, res) => {
-    // TODO: add a thing for optional endpoint ending
     const { team_key, year, aep } = req.body;
-    const response = await getTBA(`/team/${team_key}/matches/${year}${aep}`);
+    const response = await getTBA(`/team/${team_key}/events/${year}${aep}`);
     res.json(response);
 });
 const getTBA = async (endpoint) => {
